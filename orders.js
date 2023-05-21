e0eb851a9ca9798438ab4af02501a37a5b73e896
KapilShrestha21 <130133864+KapilShrestha21@users.noreply.github.com>
2023-05-21 17:26:40 +0545

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,32 @@
+const Orders = [
+{
+    productName: 'Dog smiling painting',
+    productNumber: '8745',
+    paymentStatus: 'Paid',
+    shipping: 'Delivered'
+},
+{
+    productName: 'Human with cat',
+    productNumber: '9056',
+    paymentStatus: 'Due',
+    shipping: 'Delivered'
+},
+{
+    productName: 'Mountain painting',
+    productNumber: '5412',
+    paymentStatus: 'Refunded',
+    shipping: 'Pending'
+},
+{
+    productName: 'Three trees',
+    productNumber: '3455',
+    paymentStatus: 'Paid',
+    shipping: 'Declined'
+},
+{
+    productName: 'Tall man',
+    productNumber:'6865',
+    paymentStatus: 'Paid',
+    shipping: 'Delivered'
+}
+]
